444949c3c085e873e41f450d75e47a30d2b83494
giacomoratta <giacomo.ratta@diymaxeda.com>
2021-11-16 08:05:28 +0100

    lect-41 tests with query for popover summaryform

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -30,3 +30,23 @@ test('Checkbox disables button on first click and enables on second click', () =
   userEvent.click(checkbox)
   expect(confirmButton).toBeDisabled()
 })
+
+test('popover responds to hover', () => {
+  render(<SummaryForm />)
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(/no ice cream will be actually delivered/i)
+  expect(nullPopover).not.toBeInTheDocument()
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  userEvent.hover(termsAndConditions)
+
+  const popover = screen.queryByText(/no ice cream will be actually delivered/i)
+  expect(popover).toBeInTheDocument() // optional but needed for tests' clarity
+
+  // popover disappears when we move out
+  userEvent.unhover(termsAndConditions)
+  const nullPopoverAgain = screen.queryByText(/no ice cream will be actually delivered/i)
+  expect(nullPopoverAgain).not.toBeInTheDocument()
+})
