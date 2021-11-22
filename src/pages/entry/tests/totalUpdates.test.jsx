2e3323030f9ce530915e8ec10969d2fc150c1a7b
giacomoratta <giacomo.ratta@diymaxeda.com>
2021-11-22 10:30:44 +0100

    lect-64 sundae - added tests for topping subtotal

--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -23,3 +23,25 @@ test('update scoop subtotal when scoops change', async () => {
   userEvent.type(chocolateInput, '2')
   expect(scoopsSubtotal).toHaveTextContent('6.00')
 })
+
+test('update topping subtotal when toppings change', async () => {
+  render(<Options optionType='toppings' />)
+
+  // make sure total starts out $0.00
+  const toppingsSubtotal = screen.getByText('Toppings total: $', { exact: false })
+  expect(toppingsSubtotal).toHaveTextContent('0.00')
+
+  // add cherries topping
+  const cherriesInput = await screen.findByRole('checkbox', { name: 'Cherries'})
+  userEvent.click(cherriesInput)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+
+  // add hot fudge topping
+  const hotFudgeInput = await screen.findByRole('checkbox', { name: 'Hot fudge'})
+  userEvent.click(hotFudgeInput)
+  expect(toppingsSubtotal).toHaveTextContent('3.00')
+
+  // remove hot fudge topping
+  userEvent.click(hotFudgeInput)
+  expect(toppingsSubtotal).toHaveTextContent('1.50')
+})
