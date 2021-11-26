b664939506e3096c080fe76ad7d65c659eb25946
giacomoratta <giacomo.ratta@diymaxeda.com>
2021-11-26 09:34:11 +0100

    lect-85 sundae - handles error on order confirmation page

--- src/pages/confirmation/OrderConfirmation.jsx
+++ src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Button from 'react-bootstrap/Button'
 import { useOrderDetails } from '../../contexts/OrderDetails'
+import AlertBanner from '../common/AlertBanner'
 
 export default function OrderConfirmation ({ setOrderPhase }) {
   const [, , resetOrder] = useOrderDetails()
   const [orderNumber, setOrderNumber] = useState(null)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     axios
@@ -15,8 +17,14 @@ export default function OrderConfirmation ({ setOrderPhase }) {
       .then((response) => {
         setOrderNumber(response.data.orderNumber)
       })
-      .catch((error) => { /* TODO */ })
+      .catch((error) => {
+        setError(true)
+      })
   }, [])
+
+  if (error) {
+    return <AlertBanner message={null} variant={null} />
+  }
 
   function handleClick () {
     // clear the order details
